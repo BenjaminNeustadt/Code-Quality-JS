aaa148c3781cbd8c3c2307e63551ca4aa823be76
benjamin <b.james.neustadt@gmail.com>
2022-07-28 14:29:03 +0200

    Test passes for multiple credit

--- ledger.test.js
+++ ledger.test.js
@@ -14,23 +14,31 @@ const Ledger = require('./ledger');
 
 describe('Ledger', () => {
 
+  beforeEach(() => {
+    ledger = new Ledger();
+    format = ledger.formatReport();
+  })
+
   it('formatReport method should return empty at first', () => {
-    let ledger = new Ledger();
     expect(ledger.formatReport()).toEqual([]);
   })
 
   it('when account credited, returns report with headers: formatted date and increment balance', () => {
-    let ledger = new Ledger();
     ledger.credit(100)
-    let format = ledger.formatReport();
     expect(format).toEqual([{date: new Date().toLocaleDateString(), credit: 100, debit: 0, balance: 100}])
   })
 
   it('when account debited, returns report with headers: formatted date and increment balance', () => {
-    let ledger = new Ledger();
     ledger.debit(100)
-    let format = ledger.formatReport();
     expect(format).toEqual([{date: new Date().toLocaleDateString(), credit: 0, debit: 100, balance: 100}])
   })
+
+  it('increments balance, and new event cases for multiple entries', () => {
+    ledger.debit(100)
+    ledger.debit(800)
+    expect(format).toEqual([{date: new Date().toLocaleDateString(), credit: 0, debit: 100, balance: 100}, {date: new Date().toLocaleDateString(), credit: 0, debit: 800, balance: 900}])
+  })
+
+
 
 })

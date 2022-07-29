b4520e89cec82deee50228bf4eb040e12a03fc12
Benjamin <b.james.neustadt@gmail.com>
2022-07-29 14:03:53 +0200

    Reformat debit/credit methods

--- ledger.js
+++ ledger.js
@@ -7,12 +7,22 @@ class Ledger {
 
   credit(amount){
     this.balance += amount
-    this.history.push({date: this.date, credit: amount, debit: 0, balance: this.balance})
+    this.history.push({
+      date: this.date,
+      credit: amount,
+      debit: 0,
+      balance: this.balance
+    })
   }
 
   debit(amount){
     this.balance -= amount
-    this.history.push({date: this.date, credit: 0, debit: amount , balance: this.balance})
+    this.history.push({
+      date: this.date,
+      credit: 0,
+      debit: amount,
+      balance: this.balance
+    })
   }
 
   formatReport(){
